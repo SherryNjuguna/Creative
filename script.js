c205df23000e5605e18f27bf279d539884730bcd
charlesnyanje <charleswilliam26@gmail.com>
2023-03-08 12:53:15 +0300

    added new images

--- script.js
+++ script.js
@@ -12,6 +12,6 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}    
     slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 3000); // Change image every 2 seconds
+    setTimeout(showSlides, 6000); // Change image every 2 seconds
 }
 
